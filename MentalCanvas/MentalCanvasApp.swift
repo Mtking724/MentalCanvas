1e88429227d4722b825b3903cab8af80879ca9ff
motoki0724 <motokiokayasu@gmail.com>
2023-11-26 22:26:56 +0900

    Initial Commit

---
+++ MentalCanvas/MentalCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalCanvasApp.swift
+//  MentalCanvas
+//
+//  Created by Motoki Okayasu on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MentalCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
---
+++ View/StartView.swift
@@ -0,0 +1,33 @@
+//
+//  StartView.swift
+//  MentalCanvas
+//
+//  Created by Motoki Okayasu on 2023/11/26.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        ZStack{
+            Color.mint
+                .ignoresSafeArea()
+
+            VStack{
+                Image("artMan")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200 )
+                Text("MentalCanvasへようこそ")
+                    .padding(.top)
+                    .offset(y: 30)
+                    .foregroundColor(.white)
+                    .font(.title2.bold())
+                
+            }
+        }
+    }
+}
+#Preview {
+    StartView()
+}
